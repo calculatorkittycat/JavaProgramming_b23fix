79dec4ccd172dacc76bb97ad9d448b06d0ddba7c
calculatorkittycat <cicain90@gmail.com>
2021-07-22 09:13:53 -0700

    day34

---
+++ src/day_34_LocalDateTime_Wrapper/DateTimeFormatting.java
@@ -0,0 +1,38 @@
+package day_34_LocalDateTime_Wrapper;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeFormatting {
+
+    public static void main(String[] args) {
+
+        DateTimeFormatter DF = DateTimeFormatter.ofPattern("MM/dd/YYYY/EEEE");
+        LocalDate today = LocalDate.now();
+
+        System.out.println(today);
+
+        today.format(DF);
+
+        System.out.println(today.format(DF));
+
+        System.out.println("______________________________________________");
+
+        DateTimeFormatter TF = DateTimeFormatter.ofPattern("hh:mm a");
+        LocalTime currentTime = LocalTime.now();
+
+        System.out.println(currentTime);
+        System.out.println(currentTime.format(TF));
+
+        LocalTime time1 = LocalTime.of(17,25);
+        System.out.println(time1);
+
+        System.out.println(time1.format(TF));
+
+    }
+}
+
+
+
+//MM/dd/YYYY
---
+++ src/day_34_LocalDateTime_Wrapper/LocalTimeIntro.java
@@ -0,0 +1,19 @@
+package day_34_LocalDateTime_Wrapper;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class LocalTimeIntro {
+
+    public static void main(String[] args) {
+
+        LocalTime now = LocalTime.now();
+
+        System.out.println(now);
+
+        LocalTime breakTime = now.plusMinutes(15);
+
+        System.out.println(breakTime);
+
+    }
+}
